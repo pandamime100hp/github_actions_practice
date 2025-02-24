822e18406c751c439da77ea078a6787b26631647
Makar Emeliyanov <66727639+Pandamime100hp@users.noreply.github.com>
2025-02-24 09:53:39 +0100

    fix(tests): fixed test from equals to equal.

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -6,4 +6,4 @@ from src.hello_world import hello_world
 class TestMain(unittest.TestCase):
     def test_main(self):
         output = hello_world()
-        self.assertEquals(output, "Hello World")
+        self.assertEqual(output, "Hello World")
